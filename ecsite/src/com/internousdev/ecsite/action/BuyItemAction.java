edb5d17647be37d2be191d392500c86b61df7efc
tnee <ttkgy1212@gmail.com>
2018-10-31 14:48:14 +0900

    ECサイト中級

--- ecsite/src/com/internousdev/ecsite/action/BuyItemAction.java
+++ ecsite/src/com/internousdev/ecsite/action/BuyItemAction.java
@@ -19,10 +19,10 @@ public class BuyItemAction extends ActionSupport implements SessionAware{
 		session.put("total_price",intCount * intPrice);
 		String payment;
 		if(pay.equals("1")){
-			payment="現金払ぁE;
+			payment="現金払い";
 			session.put("pay",payment);
 		}else{
-			payment="クレジチE��カーチE;
+			payment="クレジットカード";
 			session.put("pay", payment);
 		}
 		return result;
